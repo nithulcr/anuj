35816daad03856e16cbd694aa6a6babedfe5cf1a
nithul <nithulcr@gmail.com>
2025-05-31 12:05:47 +0530

    second_commit31

--- js/js.js
+++ js/js.js
@@ -46,88 +46,8 @@ document.querySelector(".switch").addEventListener("click", function () {
   window.onload = initializeDarkMode;
 
 
-//   cursor script start
-if (window.innerWidth > 768) {
-    var $cursor = $('.cursor');
-    var $cursorScale = $('.cursor-scale');
-    var mouseX = 0;
-    var mouseY = 0;
-
-    $(window).on('mousemove', function (e) {
-        mouseX = e.clientX;
-        mouseY = e.clientY;
-
-        // Animate the cursor position with jQuery
-        $cursor.css({
-            left: mouseX + 'px',
-            top: mouseY + 'px'
-        });
-    });
-
-    $cursorScale.each(function () {
-        var $link = $(this);
-
-        $link.on('mousemove', function () {
-            $cursor.addClass('grow'); 
-            if ($link.hasClass('small')) {
-                $cursor.removeClass('grow').addClass('grow-small');
-            }
-        });
-
-        $link.on('mouseleave', function () {
-            $cursor.removeClass('grow').removeClass('grow-small');
-        });
-    });
-}
 
 
-
-// typing cursor for banner
-const typedTextSpan = document.querySelector(".typed-text");
-const cursorSpan = document.querySelector(".cursor_text");
-
-const textArray = ["Project Manager", "PMO Professional", "Certified Scrum Master"];
-const typingDelay = 200;
-const erasingDelay = 100;
-const newTextDelay = 2000; // Delay between current and next text
-let textArrayIndex = 0;
-let charIndex = 0;
-
-function type() {
-  if (charIndex < textArray[textArrayIndex].length) {
-    if (!cursorSpan.classList.contains("typing"))
-      cursorSpan.classList.add("typing");
-    typedTextSpan.textContent += textArray[textArrayIndex].charAt(charIndex);
-    charIndex++;
-    setTimeout(type, typingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    setTimeout(erase, newTextDelay);
-  }
-}
-
-function erase() {
-  if (charIndex > 0) {
-    if (!cursorSpan.classList.contains("typing"))
-      cursorSpan.classList.add("typing");
-    typedTextSpan.textContent = textArray[textArrayIndex].substring(
-      0,
-      charIndex - 1
-    );
-    charIndex--;
-    setTimeout(erase, erasingDelay);
-  } else {
-    cursorSpan.classList.remove("typing");
-    textArrayIndex++;
-    if (textArrayIndex >= textArray.length) textArrayIndex = 0;
-    setTimeout(type, typingDelay + 1100);
-  }
-}
-
-document.addEventListener("DOMContentLoaded", function () {
-  // On DOM Load initiate the effect
-  if (textArray.length) setTimeout(type, newTextDelay + 250);
-});
 
 
 // swiper start
